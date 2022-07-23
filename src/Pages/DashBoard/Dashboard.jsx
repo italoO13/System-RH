dafcbdae84de3f6ced1912430ce7dccbef1b80ef
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-07-23 09:36:00 -0300

    :boom: fix: Revertendo grafico de admitido vs demitido, por enquanto ficará comentado até saber se realmente colocarei

--- src/Pages/DashBoard/Dashboard.jsx
+++ src/Pages/DashBoard/Dashboard.jsx
@@ -15,7 +15,7 @@ const DashBoard= () => {
       }
     }
     changeRender();
-  },[funcionarios])
+  },[funcionarios]);
 
   const graphGender = () => {
     const values = _.groupBy(funcionarios, (func) => {
@@ -35,7 +35,7 @@ const DashBoard= () => {
       height={"400px"}
     />
     )
-  }
+  };
 
   const graphSalary = () => {
     const values = _.groupBy(funcionarios, (func) => {
@@ -68,26 +68,73 @@ const DashBoard= () => {
         options={options}
       />
     )
-  }
+  };
 
   const indicators =() =>{
-    console.log(funcionarios);
     const totalFuncionario = funcionarios
       .filter((func) => func.dataDesligamento === '').length;
     const funcionariosDesligados = funcionarios
       .filter((func) => func.dataDesligamento !== '').length;
-    
-  }
+    return(
+      <div className="d-flex justify-content-center">
+        <div>
+          <h1>{totalFuncionario}</h1>
+          <p>Funcionários Ativos</p>
+        </div>
+        <div>
+          <h1>{funcionariosDesligados}</h1>
+          <p>Total de Demissões</p>
+        </div>
+      </div>
+    );
+  };
+
+  // const graphEmployees = () =>{
+  //   const Admissao = funcionarios.map((func) => {
+  //     const ano = new Date(func.dataAdmissao).getFullYear();
+  //     const mes = new Date(func.dataAdmissao).toLocaleString('pt-br', {month: "long"});
+  //     return [ano, mes, 1 , 0];
+  //   });
+  //   const Demissao = funcionarios.filter((obj) => obj.dataDesligamento !== '').map((func) => {
+  //       const ano = new Date(func.dataDesligamento).getFullYear();
+  //       const mes = new Date(func.dataDesligamento).toLocaleString('pt-br', {month: "long"});
+  //       return [ano, mes, 0, 1];
+  //   });
+
+  //  const v = funcionarios.map((func) => {
+  //   const {dataAdmissao, dataDesligamento} = func;
+  //   if(dataAdmissao !='' && dataDesligamento)
+  //  })
+
+  //   const data = [...Admissao, ...Demissao]
+  //   const options = {
+  //     isStacked: true,
+  //     height: 300,
+  //     legend: { position: "top", maxLines: 3 },
+  //   }
+  //   console.log(funcionarios)
+  //   // [ano, mes,admissao,demissao]
+  //   return (
+  //     <Chart
+  //     chartType="AreaChart"
+  //     width="100%"
+  //     height="400px"
+  //     data={[['ano', 'mes', 'Admissão', 'Demissão'], ...data]}
+  //     options={options}
+  //   />
+  //   )
+  // };
 
   return(
-    <div>
+    <div >
       <NavBar />
       <h1>Dashboard</h1>
-      <div className="d-flex">
-      {statusRender && graphGender()};
-      {statusRender && graphSalary()}
-      </div>
       {statusRender && indicators()}
+      <div className="d-flex">
+      {statusRender && graphGender()}
+      {statusRender && graphSalary()}
+      {/* {statusRender && graphEmployees()} */}
+      </div>
 
     </div>
   )
